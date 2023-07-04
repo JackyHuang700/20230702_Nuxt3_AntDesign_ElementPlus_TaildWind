81379291c48fce4cfd75d5a2b99b436e4f49ac5d
Jacky <jacky8595125@gmail.com>
2023-07-04 21:47:47 +0800

    add: add anchor component

---
+++ components/antDesignVue/anchor/AntDAnchor.vue
@@ -0,0 +1,49 @@
+<template>
+   <a-anchor v-bind="{...$attrs, ...props}">
+    <slot />
+   </a-anchor>
+</template>
+<script setup lang="ts">
+import { computed, reactive } from 'vue'
+import type { AnchorProps } from 'ant-design-vue'
+
+interface IProps {
+}
+
+const props = defineProps<IProps & IAntDAnchor['attrs'] & IAntDAnchor['on']>()
+
+</script>
+
+<script lang="ts">
+export default {
+  name: 'AntDAnchor',
+}
+import { merge } from 'lodash-es'
+
+interface IAntDAnchor {
+  attrs?: {
+    class?: string
+    targetOffset?: AnchorProps['targetOffset']
+    getCurrentAnchor?: AnchorProps['getCurrentAnchor']
+    affix?: AnchorProps['affix']
+    bounds?: AnchorProps['bounds']
+  }
+  on?: {
+    click: AnchorProps['onClick']
+    change: AnchorProps['onChange']
+  }
+}
+
+export function useAntDComponent(_data_initConfig?: IAntDAnchor): IAntDAnchor {
+  const data_defaultSettings = reactive<IAntDAnchor>({
+    attrs: {
+      affix: false,
+    },
+  })
+  const data_init = merge(data_defaultSettings, _data_initConfig)
+
+  return data_init
+}
+</script>
+<style scoped lang="css">
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,28 +6,25 @@
     </section>
     <section class="ant-design-vue">
       <div class="collapse">
-        <AntDCollapse v-model:activeKey="collapse_tem.activeKey" v-bind="{ ...collapse_tem.attrs }"
-          v-on="{ ...collapse_tem.on }">
+        <AntDCollapse
+          v-model:activeKey="collapse_tem.activeKey"
+          v-bind="{ ...collapse_tem.attrs }"
+          v-on="{ ...collapse_tem.on }"
+        >
           <a-collapse-panel key="1">
-            <template #header>
-              This is panel header 1
-            </template>
+            <template #header> This is panel header 1 </template>
             <template #default>
               <p>{{ 'copplse title 1' }}</p>
             </template>
           </a-collapse-panel>
           <a-collapse-panel key="2" :disabled="true">
-            <template #header>
-              This is panel header 2
-            </template>
+            <template #header> This is panel header 2 </template>
             <template #default>
               <p>{{ 'copplse title 2' }}</p>
             </template>
           </a-collapse-panel>
           <a-collapse-panel key="3">
-            <template #header>
-              This is panel header 3
-            </template>
+            <template #header> This is panel header 3 </template>
             <template #default>
               <p>{{ 'copplse title 3' }}</p>
             </template>
@@ -35,11 +32,14 @@
         </AntDCollapse>
       </div>
       <div class="p-4 card">
-        <AntDCard v-bind="{
-          ...card_tem.attrs
-        }" v-on="{
-  ...card_tem.on
-}">
+        <AntDCard
+          v-bind="{
+            ...card_tem.attrs,
+          }"
+          v-on="{
+            ...card_tem.on,
+          }"
+        >
           <template #title>
             <!-- FIXME: -->
             <div>title</div>
@@ -54,6 +54,13 @@
           </template>
         </AntDCard>
       </div>
+      <div class="p-3 anchor">
+        <AntDAnchor v-bind="{ ...anchor_tem.attrs }" v-on="{ ...anchor_tem.on }">
+          <a-anchor-link href="#components-anchor-basic" title="Basic demo" />
+          <a-anchor-link href="#components-anchor-static" title="Static demo" />
+          <a-anchor-link href="#components-anchor-basic2" title="Basic demo with Target" target="_blank" />
+        </AntDAnchor>
+      </div>
     </section>
     <section class="element-plus">
       <el-button @click="ElMessage('hello')">button</el-button>
@@ -62,8 +69,12 @@
     </section>
     <section class="recaptcha">
       <ClientOnly>
-        <a href="https://hackmd.io/@C_ui2IwiRJm8iHGDtIvecw/B1BG-lxti?utm_source=preview-mode&utm_medium=rec"
-          target="_blank" rel="noopener noreferrer">Ben Li</a>
+        <a
+          href="https://hackmd.io/@C_ui2IwiRJm8iHGDtIvecw/B1BG-lxti?utm_source=preview-mode&utm_medium=rec"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Ben Li</a
+        >
         <Recaptcha />
       </ClientOnly>
     </section>
@@ -75,22 +86,17 @@ import Recaptcha from '@/components/recaptcha/Index.vue'
 // ant-design-vue
 import AntDCollapse, { useAntDCollapse } from '@/components/antDesignVue/collapse/AntDCollapse.vue'
 import AntDCard, { useAntDCard } from '@/components/antDesignVue/card/AntDCard.vue'
+import AntDAnchor, { useAntDComponent } from '@/components/antDesignVue/anchor/AntDAnchor.vue'
 
 // element-plus
-
-
-
-
 
 const collapse_tem = useAntDCollapse({
   activeKey: ['1', '3'],
   attrs: {
     class: '',
   },
-  on: {
-  }
+  on: {},
 })
-
 
 const card_tem = useAntDCard({
   attrs: {
@@ -98,7 +104,20 @@ const card_tem = useAntDCard({
     loading: true,
   },
 })
+
 card_tem.attrs.loading = false // FIXME:
 
+const anchor_tem = useAntDComponent({
+  attrs: {
+    class: '',
+    getCurrentAnchor: () => {
+      return '#components-anchor-basic'
+    },
+  },
+})
+// FIXME:
+anchor_tem.attrs!.getCurrentAnchor = () => {
+  return '#components-anchor-basic'
+}
 </script>
 <style scoped lang="scss"></style>
